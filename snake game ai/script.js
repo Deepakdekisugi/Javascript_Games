d908aebb10fccc8bdaf2e8be71b1b800ca3c80e3
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-06 22:12:41 +0530

    added apple food function

--- snake game ai/script.js
+++ snake game ai/script.js
@@ -13,8 +13,48 @@ class Snake {
         var newRect;
         if(this.rotateX == 1){
             newRect = {
-                x : this.tail[this.tail.length - 1].x + this.size
+                x : this.tail[this.tail.length - 1].x + this.size,
+                y : this.tail[this.tail.length - 1].y
             }
+        }else if(this.rotateX == -1){
+            newRect = {
+                x : this.tail[this.tail.length - 1].x - this.size,
+                y : this.tail[this.tail.length - 1].y
+            }
+        }else if(this.rotateY == 1){
+            newRect = {
+                x : this.tail[this.tail.length - 1].x,
+                y : this.tail[this.tail.length - 1].y + this.size
+            }
+        }else if(this.rotateX == -1){
+            newRect = {
+                x : this.tail[this.tail.length - 1].x,
+                y : this.tail[this.tail.length - 1].y - this.size
+            }
+        }
+
+        this.tail.shift()
+        this.tail.push(newRect)
+    }
+}
+
+class Apple {
+    constructor() {
+        var isTouching;
+        while(true) {
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            for ( var i = 0; i < snake.tail.length; i++) {
+                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+                    isTouching = true;
+                }
+            }
+            if (!isTouching) {
+                break;
+            }
+            this.color = "pink"
+            this.size = snake.size
         }
     }
 }
