864c60f21f5e2d87e1b0cdb08ae225266fbc5ff4
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-14 22:28:40 +0530

    completed the game and added readme.md

---
+++ Black Jack Game/readme.md
@@ -0,0 +1,44 @@
+# Blackjack Game
+
+The Blackjack Game is a web-based implementation of the popular card game Blackjack (also known as 21), created using HTML, CSS, and JavaScript. It allows players to play against the dealer and experience the thrill of trying to beat the house in a virtual casino environment.
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+2. The game will be displayed with a virtual table, player hands, and dealer hand.
+3. Each player (including the dealer) is initially dealt two cards.
+4. The player's objective is to get a hand value as close to 21 as possible without going over.
+5. The player can choose to "Hit" (draw another card) or "Stand" (end their turn) by clicking the respective buttons.
+6. If the player's hand value exceeds 21, they go "Bust" and lose the round.
+7. Once the player stands, the dealer reveals their facedown card and draws cards until their hand value reaches at least 17.
+8. The dealer must stand if their hand value is 17 or higher.
+9. If the dealer's hand value exceeds 21, they go "Bust," and the player wins the round.
+10. If neither the player nor the dealer busts, the hands are compared, and the one closest to 21 without exceeding it wins the round.
+11. The game continues with subsequent rounds until the player decides to quit or runs out of chips.
+
+## Files
+
+The project contains the following files:
+
+- `index.html`: The main HTML file that contains the structure of the game and handles user interactions.
+- `styles.css`: The CSS file that provides styles and layout for the game.
+- `script.js`: The JavaScript file that implements the game logic, including card dealing, player actions, dealer AI, and scorekeeping.
+
+## Customization
+
+You can customize the Blackjack Game by modifying the following aspects:
+
+- Styling: Feel free to modify the CSS file to change the appearance of the game, such as colors, fonts, or layout, to match your preferences.
+- Game Rules: The game rules, such as the number of decks used, dealer's behavior, or payout ratios, can be adjusted in the JavaScript code to suit your desired variation of Blackjack.
+
+## Browser Compatibility
+
+The Blackjack Game is built using standard HTML, CSS, and JavaScript and should be compatible with most modern web browsers, including Chrome, Firefox, Safari, and Edge.
+
+## License
+
+This project is licensed under the MIT License. You are free to use, modify, and distribute the code as per the terms of the license.
+
+## Acknowledgments
+
+The creation of this Blackjack Game was inspired by the classic game played in casinos worldwide. Special thanks to the developers and contributors of online resources and tutorials that have provided valuable insights into game development techniques using web technologies.
--- Black Jack Game/script.js
+++ Black Jack Game/script.js
@@ -1,4 +1,4 @@
-let dealerSum =0;
+let dealerSum = 0;
 let yourSum = 0;
 
 let dealerAceCount = 0;
@@ -9,9 +9,10 @@ let deck;
 
 let canHit = true; //draw while sum is less then 21
 
-window.onload = function() {
+window.onload = function () {
     buildDeck();
     shuffleDeck();
+    startGame();
 }
 
 function buildDeck() {
@@ -20,7 +21,7 @@ function buildDeck() {
     deck = [];
 
     for (let i = 0; i < types.length; i++) {
-        for(let j = 0; j < values.length; j++) {
+        for (let j = 0; j < values.length; j++) {
             deck.push(values[j] + "-" + types[i]);
         }
     }
@@ -35,4 +36,103 @@ function shuffleDeck() {
         deck[j] = temp;
     }
     console.log(deck);
+}
+
+function startGame() {
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+
+    while (dealerSum < 17) {
+
+        let cardImg = document.createElement("img"); //<img location>
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealerSum);
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+    }
+    console.log(yourSum);
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+}
+
+function hit() {
+    if (!canHit) {
+        return;
+    }
+
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    yourSum += getValue(card);
+    yourAceCount += checkAce(card);
+    document.getElementById("your-cards").append(cardImg);
+
+    if(reduceAce(yourSum, yourAceCount) > 21) {
+        canHit = false;
+    }
+}
+
+function stay() {
+    dealerSum = reduceAce(dealerSum, dealerAceCount);
+    yourSum = reduceAce(yourSum, yourAceCount);
+
+    canHit = false;
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+
+    let message = "";
+    if(yourSum > 21) {
+        message = "You Lose!";
+    } else if(dealerSum > 21) {
+        message = "You Win!";
+    }else if(yourSum == dealerSum) {
+        message = "Tie!";
+    }else if(yourSum > dealerSum) {
+        message = "You Win!";
+    }else if(yourSum < dealerSum) {
+        message = "You Lose!";
+    }
+
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("your-sum").innerText = yourSum;
+    document.getElementById("results").innerText = message;
+}
+
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+    if (isNaN(value)) {
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
+
+function reduceAce(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
+    }
+    return playerSum;
 }
