ee5e2a2231d49effcc399cc3eedb11daf89df2e6
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-14 20:36:11 +0530

    added reshuffle function

--- Black Jack Game/script.js
+++ Black Jack Game/script.js
@@ -11,10 +11,28 @@ let canHit = true; //draw while sum is less then 21
 
 window.onload = function() {
     buildDeck();
+    shuffleDeck();
 }
 
 function buildDeck() {
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"];
-    deck
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for(let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]);
+        }
+    }
+    //console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
 }
