102487f5e794a2e3d572bcc7af24e4a4b6b1d1a3
Deepak <dk376907@gmail.com>
2024-08-26 23:20:01 +0530

    Update script.js

--- Road_Cross/script.js
+++ Road_Cross/script.js
@@ -12,3 +12,23 @@ const camera = new THREE.OrthographicCamera(
   0.1,
   10000
 );
+
+camera.rotation.x = (50 * Math.PI) / 180;
+camera.rotation.y = (20 * Math.PI) / 180;
+camera.rotation.z = (10 * Math.PI) / 180;
+
+const initialCameraPositionY = -Math.tan(camera.rotation.x) * distance;
+const initialCameraPositionX =
+  Math.tan(camera.rotation.y) *
+  Math.sqrt(distance ** 2 + initialCameraPositionY ** 2);
+camera.position.y = initialCameraPositionY;
+camera.position.x = initialCameraPositionX;
+camera.position.z = distance;
+
+const zoom = 2;
+
+const chickenSize = 15;
+
+const positionWidth = 42;
+const columns = 17;
+const boardWidth = positionWidth * columns;
