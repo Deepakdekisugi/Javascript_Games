b299b1780f0dbd71104eb9cf0ba350466fa79121
Deepak <dk376907@gmail.com>
2024-08-26 23:22:15 +0530

    created canvas

--- Road_Cross/script.js
+++ Road_Cross/script.js
@@ -137,3 +137,26 @@ const initaliseValues = () => {
 };
 
 initaliseValues();
+
+const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  
+  function Texture(width, height, rects) {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, width, height);
+    context.fillStyle = "rgba(0,0,0,0.6)";
+    rects.forEach((rect) => {
+      context.fillRect(rect.x, rect.y, rect.w, rect.h);
+    });
+    return new THREE.CanvasTexture(canvas);
+  }
